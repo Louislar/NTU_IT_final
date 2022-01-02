2f5e2be6acf7dcca9195479a358093bd26809c10
LouisLiang <a8945267@gmail.com>
2022-01-02 20:47:34 +0800

    Finish missing value generator
    完成missing value generator，但是尚未測試功能的運作是否完全正常或是符合預期。
    E.g. 是否到達預期的missing rate，以及incomple ratio是否如輸入的參數相同

--- miss_generator.py
+++ miss_generator.py
@@ -19,7 +19,7 @@ def gen_rand_nums_given_sum(num_rv, sum):
     '''
     if num_rv>sum:
         return "error"
-    cur_sum=sum-num_rv
+    cur_sum = num_rv
     rv_arr = np.full(num_rv,1)
     counter=0
 
@@ -41,19 +41,26 @@ def generate_missing_value_mask(num_tuples, num_attr, miss_rate, complete_ratio)
     When number of incomplete tuples greater than number of missing items, an error occurs. 
     '''
     num_incomplete_tuples = np.around(num_tuples*(1-complete_ratio), decimals=0).astype(int)
-    print(num_incomplete_tuples)
-    num_miss_items = num_tuples*num_attr*miss_rate
+    num_miss_items = np.around(num_tuples*num_attr*miss_rate).astype(int)
     if num_incomplete_tuples>num_miss_items: 
         return "error"
     miss_mask = np.full((num_tuples, num_attr), False)
-    incomplete_tuples_idx = np.random.choice(num_tuples, num_incomplete_tuples)
-    # TODO: Use gen_rand_nums_given_sum and shuffle the trturn array, 
+    incomplete_tuples_idx = np.random.choice(num_tuples, num_incomplete_tuples, replace=False) # random select without replacement!!
+    # TODO: Use gen_rand_nums_given_sum and shuffle the return array, 
     # than apply relevent missing values to the miss_mask
-    
-    # print(miss_mask)
+    print(num_incomplete_tuples)
+    print(num_miss_items)
+    num_miss_items_distribution = gen_rand_nums_given_sum(num_incomplete_tuples, num_miss_items)
+    np.random.shuffle(num_miss_items_distribution)    
+
+    for _tuple_idx, _num_miss in zip(incomplete_tuples_idx, num_miss_items_distribution): 
+        _miss_attr_idx = np.random.choice(num_attr, _num_miss, replace=False)  # random select without replacement!!
+        miss_mask[_tuple_idx, _miss_attr_idx] = True
+        print(_tuple_idx, ', ', _num_miss, ', ', _miss_attr_idx)
+    print(miss_mask)
 
 if __name__=="__main__": 
     # 10 miss items, 10 complete tupeles
     generate_missing_value_mask(20, 5, 0.1, 0.5)
 
-    gen_rand_nums_given_sum(5,10)
+    # gen_rand_nums_given_sum(5,10)
