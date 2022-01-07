956e54e745fc42b462fb272c99b725467063c549
louisLiang <a8945267@gmail.com>
2022-01-07 11:47:55 +0800

    Finished comparison figure of KNN and improved k-means
    完成對於KNN與improved k-means的比較繪圖
    實驗的方式可能要再加入不同random seed的結果平均，這樣的結果可能會比較準確

--- KNN_imputation.py
+++ KNN_imputation.py
@@ -10,46 +10,70 @@ from distance_measurement import nan_euclidean_with_categorical
 from KNN_impute_categorical import KNN_impute_voting
 
 
-# Get dataset
-full_X, miss_X, miss_mask = load_iris_miss(0.1, 0.5)
-# Without categrical attribute
-# full_X = full_X[:, :-1]
-# miss_X = miss_X[:, :-1]
-# miss_mask = miss_mask[:, :-1]
+def KNN_impute(k, miss_data, cat_mask):
+    '''
+    use euclidean distance, and ignore nan
+    numerical missing data use mean, categorical missing data use mode
+    '''
+    # numerical imputation
+    imputer = KNNImputer(
+        n_neighbors=k, 
+        metric=lambda X,Y,**kwds: paired_nan_euclidean_with_categorical(X,Y,categorical_mask=cat_mask,**kwds)
+    )
+    X_pred_numerical = imputer.fit_transform(miss_data)
+    # categorical imputation
+    X_pred_cat = KNN_impute_voting(
+        k=k, 
+        dataset=miss_data, 
+        pairwise_dis_func=lambda X,Y: nan_euclidean_with_categorical(X, Y, np.nan, cat_mask)
+    )
+    # Merge categorical predict into numerical prediction
+    cat_mask_expand = np.repeat(cat_mask[np.newaxis, :], X_pred_numerical.shape[0], axis=0)
+    X_pred_numerical[cat_mask_expand] = X_pred_cat[cat_mask_expand]
+    return X_pred_numerical
+    
 
-# Generate categorical mask
-# (In iris dataset the last column is categorical)
-cat_mask = np.full(full_X.shape[1], False)
-cat_mask[-1] = True
+if __name__=='__main__': 
+    # Get dataset
+    full_X, miss_X, miss_mask = load_iris_miss(0.1, 0.5)
+    # Without categrical attribute
+    # full_X = full_X[:, :-1]
+    # miss_X = miss_X[:, :-1]
+    # miss_mask = miss_mask[:, :-1]
 
-# Numerical imputer(mean)
-imputer = KNNImputer(
-    n_neighbors=5, 
-    metric=lambda X,Y,**kwds: paired_nan_euclidean_with_categorical(X,Y,categorical_mask=cat_mask,**kwds)
-)
-X_pred = imputer.fit_transform(miss_X)
-# print(X_pred)
-print('X_pred shape: ', X_pred[miss_mask].shape)
+    # Generate categorical mask
+    # (In iris dataset the last column is categorical)
+    cat_mask = np.full(full_X.shape[1], False)
+    cat_mask[-1] = True
 
-# Categorical imputer(voting)
-# Use KNN_impute_categorical.KNN_impute_vote impute the categorical attribute
-X_pred_cat = KNN_impute_voting(
-    k=5, 
-    dataset=miss_X, 
-    pairwise_dis_func=lambda X,Y: nan_euclidean_with_categorical(X, Y, np.nan, cat_mask)
-)
-# print(X_pred_cat)
-print('X_pred_cat shape: ', X_pred_cat.shape)
+    # Numerical imputer(mean)
+    imputer = KNNImputer(
+        n_neighbors=5, 
+        metric=lambda X,Y,**kwds: paired_nan_euclidean_with_categorical(X,Y,categorical_mask=cat_mask,**kwds)
+    )
+    X_pred = imputer.fit_transform(miss_X)
+    # print(X_pred)
+    print('X_pred shape: ', X_pred[miss_mask].shape)
 
-# Merge numerical and categorical prediction
-cat_mask_expand = np.repeat(cat_mask[np.newaxis, :], X_pred.shape[0], axis=0)
-X_pred[cat_mask_expand] = X_pred_cat[cat_mask_expand]
+    # Categorical imputer(voting)
+    # Use KNN_impute_categorical.KNN_impute_vote impute the categorical attribute
+    X_pred_cat = KNN_impute_voting(
+        k=5, 
+        dataset=miss_X, 
+        pairwise_dis_func=lambda X,Y: nan_euclidean_with_categorical(X, Y, np.nan, cat_mask)
+    )
+    # print(X_pred_cat)
+    print('X_pred_cat shape: ', X_pred_cat.shape)
+
+    # Merge numerical and categorical prediction
+    cat_mask_expand = np.repeat(cat_mask[np.newaxis, :], X_pred.shape[0], axis=0)
+    X_pred[cat_mask_expand] = X_pred_cat[cat_mask_expand]
 
 
-# Evaluation metric
-## Numerical-RMSE
-print('MSE: ', mean_squared_error(full_X[miss_mask&~cat_mask_expand], X_pred[miss_mask&~cat_mask_expand]))
-## Categorical-F1
-print(full_X[miss_mask&cat_mask_expand])
-print(X_pred[miss_mask&cat_mask_expand])
-print('F1: ', f1_score(full_X[miss_mask&cat_mask_expand], X_pred[miss_mask&cat_mask_expand], average='micro'))
+    # Evaluation metric
+    ## Numerical-RMSE
+    print('MSE: ', mean_squared_error(full_X[miss_mask&~cat_mask_expand], X_pred[miss_mask&~cat_mask_expand]))
+    ## Categorical-F1
+    print(full_X[miss_mask&cat_mask_expand])
+    print(X_pred[miss_mask&cat_mask_expand])
+    print('F1: ', f1_score(full_X[miss_mask&cat_mask_expand], X_pred[miss_mask&cat_mask_expand], average='micro'))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.metrics import f1_score
 from dataset_prepare import load_iris_miss
 from Improved_kmeans import improved_kmeans_impute
+from KNN_imputation import KNN_impute
 
 '''
 try each imputation method and plot RMSE and f1 result
@@ -16,8 +17,8 @@ def draw_impute_eval(x, y_dict, name_title='no title', name_x='no name', final_s
     plt.xlabel('missing and incomplete rate')
     for k in y_dict: 
         plt.plot(x, y_dict[k], label=k)
+    plt.legend()
     if final_show: 
-        plt.legend()
         plt.show()
 
 # KNN imputer
@@ -34,10 +35,33 @@ if __name__=="__main__":
         '1/3_0.2','1/6_0.2', '1/2_0.3', 
         '1/3_0.3', '1/6_0.3', '1/6_0.4'
     ]
+    impute_methods = {
+        'improved k-means euclidean': lambda cat_mask, miss_X: improved_kmeans_impute(
+            3, 
+            5, 
+            cat_mask, 
+            miss_X, 
+            if_euclidean=True
+        ), 
+        'improved k-means hahalanobis': lambda cat_mask, miss_X: improved_kmeans_impute(
+            3, 
+            5, 
+            cat_mask, 
+            miss_X, 
+            if_euclidean=False
+        ), 
+        'KNN euclidean': lambda cat_mask, miss_X: KNN_impute(
+            k=5, 
+            miss_data=miss_X, 
+            cat_mask=cat_mask
+        )
+    }
     rmse_incomplete_miss = {}
     f1_incomplete_miss = {}
+    for i in impute_methods: 
+        rmse_incomplete_miss[i] = []
+        f1_incomplete_miss[i] = []
     for _incomplete_miss_rate in incomplete_miss_rate: 
-        pass
         # get data
         ## iris
         full_X, miss_X, miss_mask = load_iris_miss(_incomplete_miss_rate[1], _incomplete_miss_rate[0])
@@ -45,25 +69,38 @@ if __name__=="__main__":
         cat_mask_iris[-1] = True
         ## KDDCUP 99
 
-        # improved k-means
-        impute_result = improved_kmeans_impute(
-            3, 
-            5, 
-            cat_mask_iris, 
-            miss_X, 
-            if_euclidean=True
-        )
-
+        # improved k-means (Euclidean and mahalanobis)
+        imputed_results = {
+            _impute_method_nm: \
+                impute_methods[_impute_method_nm](cat_mask_iris, miss_X) for _impute_method_nm in impute_methods
+        }
+        # imputed_results = {
+        #     'improved k-means euclidean': improved_kmeans_impute(
+        #         3, 
+        #         5, 
+        #         cat_mask_iris, 
+        #         miss_X, 
+        #         if_euclidean=True
+        #     ), 
+        #     'improved k-means hahalanobis': improved_kmeans_impute(
+        #         3, 
+        #         5, 
+        #         cat_mask_iris, 
+        #         miss_X, 
+        #         if_euclidean=False
+        #     )
+        # }
         # Evaluation
-        cat_mask_expand = np.repeat(cat_mask_iris[np.newaxis, :], full_X.shape[0], axis=0)
-        ## RMSE
-        rmse = mean_squared_error(full_X[miss_mask&~cat_mask_expand], impute_result[miss_mask&~cat_mask_expand])
-        rmse = rmse**(1/2)
-        ## f1
-        f1 = f1_score(full_X[miss_mask&cat_mask_expand], impute_result[miss_mask&cat_mask_expand], average='micro')
-        # TODO: 改成使用dictionary儲存eval result
-        rmse_incomplete_miss['k-means euclidean'] = rmse
-        f1_incomplete_miss['k-means euclidean'] = f1
+        for _imputed_method in imputed_results: 
+            cat_mask_expand = np.repeat(cat_mask_iris[np.newaxis, :], full_X.shape[0], axis=0)
+            ## RMSE
+            rmse = mean_squared_error(full_X[miss_mask&~cat_mask_expand], imputed_results[_imputed_method][miss_mask&~cat_mask_expand])
+            rmse = rmse**(1/2)
+            ## f1
+            f1 = f1_score(full_X[miss_mask&cat_mask_expand], imputed_results[_imputed_method][miss_mask&cat_mask_expand], average='micro')
+            # TODO: 改成使用dictionary儲存eval result
+            rmse_incomplete_miss[_imputed_method].append(rmse)
+            f1_incomplete_miss[_imputed_method].append(f1)
     print(rmse_incomplete_miss)
     print(f1_incomplete_miss)
     draw_impute_eval(incomplete_miss_rate_str, rmse_incomplete_miss
